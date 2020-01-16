fd0fe7dfcf8b13b27e39052c7d5f51c0fd02f734
Natsulelou <nutshapon_lelou@hotmail.com>
2020-01-16 10:41:28 +0700

    generate home

---
+++ src/modules/home/test/routes.js
@@ -0,0 +1,228 @@
+'use strict';
+var request = require('supertest'),
+    assert = require('assert'),
+    config = require('../../../config/config'),
+    _ = require('lodash'),
+    jwt = require('jsonwebtoken'),
+    mongoose = require('mongoose'),
+    app = require('../../../config/express'),
+    Home = mongoose.model('Home');
+
+var credentials,
+    token,
+    mockup;
+
+describe('Home CRUD routes tests', function () {
+
+    before(function (done) {
+        mockup = {
+            name: 'name'
+        };
+        credentials = {
+            username: 'username',
+            password: 'password',
+            firstname: 'first name',
+            lastname: 'last name',
+            email: 'test@email.com',
+            roles: ['user']
+        };
+        token = jwt.sign(_.omit(credentials, 'password'), config.jwt.secret, {
+            expiresIn: 2 * 60 * 60 * 1000
+        });
+        done();
+    });
+
+    it('should be Home get use token', (done) => {
+        request(app)
+            .get('/api/homes')
+            .set('Authorization', 'Bearer ' + token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                done();
+            });
+    });
+
+    it('should be Home get by id', function (done) {
+
+        request(app)
+            .post('/api/homes')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                request(app)
+                    .get('/api/homes/' + resp.data._id)
+                    .set('Authorization', 'Bearer ' + token)
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) {
+                            return done(err);
+                        }
+                        var resp = res.body;
+                        assert.equal(resp.status, 200);
+                        assert.equal(resp.data.name, mockup.name);
+                        done();
+                    });
+            });
+
+    });
+
+    it('should be Home post use token', (done) => {
+        request(app)
+            .post('/api/homes')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                assert.equal(resp.data.name, mockup.name);
+                done();
+            });
+    });
+
+    it('should be home put use token', function (done) {
+
+        request(app)
+            .post('/api/homes')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                var update = {
+                    name: 'name update'
+                }
+                request(app)
+                    .put('/api/homes/' + resp.data._id)
+                    .set('Authorization', 'Bearer ' + token)
+                    .send(update)
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) {
+                            return done(err);
+                        }
+                        var resp = res.body;
+                        assert.equal(resp.data.name, update.name);
+                        done();
+                    });
+            });
+
+    });
+
+    it('should be home delete use token', function (done) {
+
+        request(app)
+            .post('/api/homes')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                request(app)
+                    .delete('/api/homes/' + resp.data._id)
+                    .set('Authorization', 'Bearer ' + token)
+                    .expect(200)
+                    .end(done);
+            });
+
+    });
+
+    it('should be home get not use token', (done) => {
+        request(app)
+            .get('/api/homes')
+            .expect(403)
+            .expect({
+                status: 403,
+                message: 'User is not authorized'
+            })
+            .end(done);
+    });
+
+    it('should be home post not use token', function (done) {
+
+        request(app)
+            .post('/api/homes')
+            .send(mockup)
+            .expect(403)
+            .expect({
+                status: 403,
+                message: 'User is not authorized'
+            })
+            .end(done);
+
+    });
+
+    it('should be home put not use token', function (done) {
+
+        request(app)
+            .post('/api/homes')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                var update = {
+                    name: 'name update'
+                }
+                request(app)
+                    .put('/api/homes/' + resp.data._id)
+                    .send(update)
+                    .expect(403)
+                    .expect({
+                        status: 403,
+                        message: 'User is not authorized'
+                    })
+                    .end(done);
+            });
+
+    });
+
+    it('should be home delete not use token', function (done) {
+
+        request(app)
+            .post('/api/homes')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                request(app)
+                    .delete('/api/homes/' + resp.data._id)
+                    .expect(403)
+                    .expect({
+                        status: 403,
+                        message: 'User is not authorized'
+                    })
+                    .end(done);
+            });
+
+    });
+
+    afterEach(function (done) {
+        Home.remove().exec(done);
+    });
+
+});
